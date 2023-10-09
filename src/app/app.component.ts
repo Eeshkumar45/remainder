3e697a786cb17d95c4f1cf4f53a2edcea7fa11b3
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2023-10-10 00:38:17 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'remainder';
+
+  requestNotificationPermission() {
+    if (Notification.permission !== 'granted') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          this.showNotification('Notification Title', 'Notification Body');
+        }
+      });
+    } else {
+      this.showNotification('Notification Title', 'Notification Body');
+    }
+  }
+
+  // Function to display a notification
+  showNotification(title: string, body: string) {
+    const notification = new Notification(title, {
+      body: body
+    });
+  }
+}
