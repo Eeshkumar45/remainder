88b612940be76a7178bdecc4f237a6f4bc88c632
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2023-10-10 21:06:59 +0530

    build 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, of , range, generate , from, interval, timer, Subscribable, Subscription} from 'rxjs';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'remainder';
   timeInterval!:number;
   notification!:Observable<number>;
   notificationSubscription!:Subscription;
+
+  ngOnInit(){
+    setInterval(() => {
+      console.log("start")
+      this.sendNotification();
+    }, 8000); // 1 minute interval
+  }
+
+  sendNotification() {
+    if ('Notification' in window) {
+      const notification = new Notification('My PWA Notification', {
+        body: 'This is your PWA notification!',
+      });
+
+      notification.onclick = () => {
+        // Handle notification click event here (e.g., navigate to a specific page)
+        console.log('Notification clicked');
+      };
+    }
+  }
 
   requestNotificationPermission() {
     if (Notification.permission !== 'granted') {
